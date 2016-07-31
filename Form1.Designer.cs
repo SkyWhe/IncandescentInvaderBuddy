02578701f12bb4d06246f6fd2acc929cae11c59e
Skyler Wheaton <skywhe@gmail.com>
2016-07-31 09:19:43 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace IncandescentInvaderBuddy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.emailbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.runBtn = new System.Windows.Forms.Button();
+            this.scanTimer = new System.Windows.Forms.Timer(this.components);
+            this.windowName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.running = new System.Windows.Forms.Label();
+            this.emailcheckbox = new System.Windows.Forms.CheckBox();
+            this.soundcheckbox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // emailbox
+            // 
+            this.emailbox.Location = new System.Drawing.Point(95, 12);
+            this.emailbox.Name = "emailbox";
+            this.emailbox.Size = new System.Drawing.Size(260, 20);
+            this.emailbox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Email: ";
+            // 
+            // runBtn
+            // 
+            this.runBtn.Location = new System.Drawing.Point(9, 76);
+            this.runBtn.Name = "runBtn";
+            this.runBtn.Size = new System.Drawing.Size(260, 46);
+            this.runBtn.TabIndex = 2;
+            this.runBtn.Text = "Run/Pause";
+            this.runBtn.UseVisualStyleBackColor = true;
+            this.runBtn.Click += new System.EventHandler(this.runBtn_Click);
+            // 
+            // scanTimer
+            // 
+            this.scanTimer.Interval = 500;
+            this.scanTimer.Tick += new System.EventHandler(this.scanTimer_Tick);
+            // 
+            // windowName
+            // 
+            this.windowName.Cursor = System.Windows.Forms.Cursors.Default;
+            this.windowName.Location = new System.Drawing.Point(95, 38);
+            this.windowName.Name = "windowName";
+            this.windowName.Size = new System.Drawing.Size(260, 20);
+            this.windowName.TabIndex = 3;
+            this.windowName.Text = "DARK SOULS III";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Window Name: ";
+            // 
+            // running
+            // 
+            this.running.AutoSize = true;
+            this.running.Location = new System.Drawing.Point(6, 125);
+            this.running.Name = "running";
+            this.running.Size = new System.Drawing.Size(67, 13);
+            this.running.TabIndex = 5;
+            this.running.Text = "Not Running";
+            // 
+            // emailcheckbox
+            // 
+            this.emailcheckbox.AutoSize = true;
+            this.emailcheckbox.Location = new System.Drawing.Point(275, 76);
+            this.emailcheckbox.Name = "emailcheckbox";
+            this.emailcheckbox.Size = new System.Drawing.Size(79, 17);
+            this.emailcheckbox.TabIndex = 6;
+            this.emailcheckbox.Text = "Send Email";
+            this.emailcheckbox.UseVisualStyleBackColor = true;
+            // 
+            // soundcheckbox
+            // 
+            this.soundcheckbox.AutoSize = true;
+            this.soundcheckbox.Location = new System.Drawing.Point(274, 105);
+            this.soundcheckbox.Name = "soundcheckbox";
+            this.soundcheckbox.Size = new System.Drawing.Size(80, 17);
+            this.soundcheckbox.TabIndex = 7;
+            this.soundcheckbox.Text = "Play Sound";
+            this.soundcheckbox.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 144);
+            this.Controls.Add(this.soundcheckbox);
+            this.Controls.Add(this.emailcheckbox);
+            this.Controls.Add(this.running);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.windowName);
+            this.Controls.Add(this.runBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.emailbox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Incandescent Invader Buddy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox emailbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button runBtn;
+        private System.Windows.Forms.Timer scanTimer;
+        private System.Windows.Forms.TextBox windowName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label running;
+        private System.Windows.Forms.CheckBox emailcheckbox;
+        private System.Windows.Forms.CheckBox soundcheckbox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,345 @@
+﻿using System.Runtime.InteropServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using tessnet2;
+using System.Net.Mail;
+using System.Media;
+
+namespace IncandescentInvaderBuddy
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+        private int left;
+        private int top;
+        private int right;
+        private int bottom;
+
+        public RECT(RECT Rectangle)
+            : this(Rectangle.Left, Rectangle.Top, Rectangle.Right, Rectangle.Bottom)
+        {
+        }
+
+        public RECT(int Left, int Top, int Right, int Bottom)
+        {
+            left = Left;
+            top = Top;
+            right = Right;
+            bottom = Bottom;
+        }
+
+        public int X
+        {
+            get { return left; }
+            set { left = value; }
+        }
+        public int Y
+        {
+            get { return top; }
+            set { top = value; }
+        }
+        public int Left
+        {
+            get { return left; }
+            set { left = value; }
+        }
+        public int Top
+        {
+            get { return top; }
+            set { top = value; }
+        }
+        public int Right
+        {
+            get { return right; }
+            set { right = value; }
+        }
+        public int Bottom
+        {
+            get { return bottom; }
+            set { bottom = value; }
+        }
+        public int Height
+        {
+            get { return bottom - top; }
+            set { bottom = value + top; }
+        }
+        public int Width
+        {
+            get { return right - left; }
+            set { right = value + left; }
+        }
+        public Point Location
+        {
+            get { return new Point(Left, Top); }
+            set
+            {
+                left = value.X;
+                top = value.Y;
+            }
+        }
+        public Size Size
+        {
+            get { return new Size(Width, Height); }
+            set
+            {
+                right = value.Width + left;
+                bottom = value.Height + top;
+            }
+        }
+
+        public static implicit operator Rectangle(RECT Rectangle)
+        {
+            return new Rectangle(Rectangle.Left, Rectangle.Top, Rectangle.Width, Rectangle.Height);
+        }
+        public static implicit operator RECT(Rectangle Rectangle)
+        {
+            return new RECT(Rectangle.Left, Rectangle.Top, Rectangle.Right, Rectangle.Bottom);
+        }
+        public static bool operator ==(RECT Rectangle1, RECT Rectangle2)
+        {
+            return Rectangle1.Equals(Rectangle2);
+        }
+        public static bool operator !=(RECT Rectangle1, RECT Rectangle2)
+        {
+            return !Rectangle1.Equals(Rectangle2);
+        }
+
+        public override string ToString()
+        {
+            return "{Left: " + left + "; " + "Top: " + top + "; Right: " + right + "; Bottom: " + bottom + "}";
+        }
+
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode();
+        }
+
+        public bool Equals(RECT Rectangle)
+        {
+            return Rectangle.Left == left && Rectangle.Top == top && Rectangle.Right == right && Rectangle.Bottom == bottom;
+        }
+
+        public override bool Equals(object Object)
+        {
+            if (Object is RECT)
+            {
+                return Equals((RECT)Object);
+            }
+            else if (Object is Rectangle)
+            {
+                return Equals(new RECT((Rectangle)Object));
+            }
+
+            return false;
+        }
+    }
+
+
+    public partial class Form1 : Form
+    {
+
+        SoundPlayer simpleSound = new SoundPlayer(@"Alarm.wav");
+        Tesseract ocr = new Tesseract();
+
+        public Form1()
+        {
+            InitializeComponent();
+            ocr.Init(null, "eng", false);
+        }
+
+
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern IntPtr GetDesktopWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern IntPtr GetWindow(IntPtr hwnd, int wFlag);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct Rect
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        [DllImport("user32.dll")]
+
+        private static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rect rect);
+
+        public static Image CaptureDesktop()
+        {
+            return CaptureWindow(GetDesktopWindow());
+        }
+
+        public static Bitmap CaptureActiveWindow()
+        {
+            return CaptureWindow(GetForegroundWindow());
+        }
+
+        public static Bitmap CaptureWindow(IntPtr handle)
+        {
+            var rect = new Rect();
+            GetWindowRect(handle, ref rect);
+            var bounds = new Rectangle(rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top);
+            var result = new Bitmap(bounds.Width, bounds.Height);
+
+            using (var graphics = Graphics.FromImage(result))
+            {
+                graphics.CopyFromScreen(new Point(bounds.Left, bounds.Top), Point.Empty, bounds.Size);
+            }
+
+            return result;
+        }
+
+
+        public static IntPtr WinGetHandle(string wName)
+        {
+            IntPtr hWnd = IntPtr.Zero;
+            foreach (Process pList in Process.GetProcesses())
+            {
+                if (pList.MainWindowTitle.Contains(wName))
+                {
+                    hWnd = pList.MainWindowHandle;
+                }
+            }
+           
+
+            return hWnd;
+        }
+
+
+        public void sendWarning(String msg1, String msg2)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("grosslyincandescentwarrior@gmail.com");
+                mail.To.Add(emailbox.Text);
+                mail.Subject = msg1;
+                mail.Body = msg2;
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("grosslyincandescentwarrior@gmail.com", "Gunther*23");
+                SmtpServer.EnableSsl = true;
+                SmtpServer.Send(mail);
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            scanTimer.Enabled = false;
+            running.Text = "Not Running";
+
+        }
+        public void scanForKeywords()
+        {
+            IntPtr point = WinGetHandle(windowName.Text);
+
+            if (point != IntPtr.Zero)
+            {
+
+                Bitmap screen = CaptureWindow(point);
+
+
+                List<tessnet2.Word> result = ocr.DoOCR(screen, Rectangle.Empty);
+                String LCtest = "";
+
+                foreach (tessnet2.Word word in result)
+                {
+
+                    LCtest = word.Text.ToLower();
+                    if (LCtest == "invading")
+                    {
+
+                        if (emailcheckbox.Checked)
+                        {
+                            sendWarning("Praise the sun!", "You are invading another world");
+                        }
+
+                        if (soundcheckbox.Checked)
+                        {
+                            simpleSound.Play();
+                        }
+
+                        break;
+
+                    }
+
+                    if (LCtest == "invaded")
+                    {
+
+                        if (emailcheckbox.Checked)
+                        {
+                            sendWarning("Praise the sun!", "You are invading another world");
+                        }
+
+                        if (soundcheckbox.Checked)
+                        {
+                            simpleSound.Play();
+                        }
+
+                        break;
+
+                    }
+
+                }
+
+                screen.Dispose();
+                ocr.Dispose();
+            }
+            else
+            {
+                running.Text = "Not Running";
+                scanTimer.Enabled = false;
+                System.Windows.Forms.MessageBox.Show("Cannot find window with this name, please double check");
+
+            }
+        }
+
+
+
+
+
+        private void runBtn_Click(object sender, EventArgs e)
+        {
+            scanTimer.Enabled = !scanTimer.Enabled;
+
+
+            if (scanTimer.Enabled == true)
+            {
+                running.Text = "Running";
+            }
+            else
+            {
+
+                running.Text = "Not Running";
+            }
+        }
+
+        private void scanTimer_Tick(object sender, EventArgs e)
+        {
+
+            scanForKeywords();
+
+        }
+    }
+}
